0cdcdb3e3bf009b33ef136027ef575b9950a8a95
lorex59 <lorex59@mail.ru>
2024-01-21 15:13:30 +1000

    homework 29

---
+++ 29practic/29.5 task2.py
@@ -0,0 +1,53 @@
+
+# Функция принимает на вход строку, которая
+# состоит из скобок трех типов: круглые, квадратные
+# и фигурные. Функция должна проверить, является ли
+# переданная последовательность скобок сбалансированной
+# или нет. Функция возвращает True, если последовательность
+# сбалансирована, и False – в противном случае.
+def is_balanced(line):
+    s = []
+    d = {'(':')','{':'}','[':']'}
+    for x in line:
+        if x in d.keys():
+            s.append(x)
+        elif len(s) != 0 and d[s[len(s)-1]] == x:
+            s.pop()
+        else:
+            s.append(x)
+            break
+    if len(s) == 0:    
+        return True
+    else:
+        return False
+
+
+
+def test_is_balanced():
+    cases = {
+        '((((((((())))))))': False,
+        '{[()]}{{}}': True,
+        '{[()]}{]{}}': False,
+        '{{{((([[[]]])))}}}': True,
+        '{}': True,
+        '(': False,
+        '(}': False,
+        '(((())))[]{}': True,
+        '((()': False,
+        '[{}{})(]': False,
+        '{[]{([[[[[[]]]]]])}}': True,
+        '{[]{([[[[[[]])]]])}}': False,
+    }
+    for i, case in enumerate(cases.keys()):
+        if is_balanced(case) == cases[case]:
+            print(f'{i}: OK')
+        else:
+            print(f'{i}: Not OK')
+
+
+def main():
+    test_is_balanced()
+
+
+if __name__ == '__main__':
+    main()
